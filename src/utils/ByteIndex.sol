2e41eacb6a2239dedbbb135c211d887796187807
v <v@v.v>
2022-06-05 15:03:05 +0530

    chore: more changes

--- src/utils/ByteIndex.sol
+++ src/utils/ByteIndex.sol
@@ -27,23 +27,33 @@ contract ByteIndex {
 
 	struct Locals {
 		bool recursing;
-		bool recursionStart;
+		bool recursionStarted;
+		bool recursable;
 		bool ignoreCurrent;
 		uint256 stop;
-        uint8 bound;
+		uint256 maxBound;
+		uint8 result;
+		uint8 next;
 	}
 
 	function lookup(
 		Index storage index,
 		bytes32 key,
-		function(bytes32, uint8) internal view returns (uint8) _search
+		function(bytes32, uint8) internal view returns (uint8) _search,
+		function(bytes32, uint256, uint256)
+			internal
+			pure
+			returns (bool, uint256) _determineRecursable,
+		function(bytes32) internal pure returns (bool) _determineRecursion
 	) internal returns (bool success, bytes32 found) {
 		uint256 shift = (index.depth - 1) / 8;
 		Locals memory locals;
+		bytes32 localsPtr;
 		assembly {
 			//preserve memory
 			found := mload(FOUND_SLOT)
 			mstore(FOUND_SLOT, 0x0)
+			localsPtr := locals
 		}
 		for (uint256 i = 0; i < shift; i += 8) {
 			bytes32 partialKey;
@@ -52,7 +62,7 @@ contract ByteIndex {
 				partialKey := or(_found, shl(0xff, sub(shift, i)))
 			}
 			bytes32 mask = index.mask[partialKey];
-			uint8 nextByte = uint8(key[i / 8]);
+			locals.next = uint8(key[i / 8]);
 			/*
 			 * exit conditions
 			 * 1. if not recursing and mask returns a 0x0
@@ -60,30 +70,98 @@ contract ByteIndex {
 			 * 3. next byte is 0 and current search is ignored and mask isn't a top bit mask
 			 */
 			if (
-				(!locals.recursing && mask == 0) ||
-				(locals.recursionStart && locals.stop == i) ||
-				(locals.ignoreCurrent && locals.nextByte == 0 && (mask & TOP == 0))
+				(!locals.recursable && mask == bytes32(0x0)) ||
+				(locals.recursionStarted && locals.stop == i) ||
+				(locals.ignoreCurrent && locals.next == 0)
 			) {
 				assembly {
 					mstore(FOUND_SLOT, found)
 				}
 				return (false, bytes32(0x0));
 			}
+			if (locals.recursionStarted && mask == bytes32(0x0)) {
+				// sanitize current slot
+				assembly {
+					let bound := shl(0xff, sub(add(shift, 8), i))
+					mstore(FOUND_SLOT, xor(or(mload(FOUND_SLOT), bound), bound))
+				}
+				i -= 16;
+				locals.recursing = true;
+				continue;
+			}
 			if (locals.recursing) {
-				nextByte++;
+				locals.next++;
 				locals.recursing = false;
 			}
-            if(mask != TOP) {
-                if()
-            }
-			uint8 result = _search(mask, nextByte);
+			uint8 result;
+			if (mask != TOP) {
+				if (!locals.recursable) {
+					locals.maxBound = 0x1 << (0xff - uint256(locals.next));
+					/*
+					 * check if pathmapping has any values in the range we want
+					 */
+					(locals.recursable, locals.recursionStop) = _determineRecursable(
+						mask,
+						locals.maxBound,
+						i
+					);
+				}
+				uint8 bound;
+				if (!locals.ignoreCurrent) bound = locals.next;
+				locals.result = 0xff - _search(mask, bound);
+			} else {
+				locals.result = 0;
+			}
+			if (_determineRecursion(localsPtr)) {
+				// sanitize current slot
+				assembly {
+					let bound := shl(0xff, sub(add(shift, 8), i))
+					mstore(FOUND_SLOT, xor(or(mload(FOUND_SLOT), bound), bound))
+				}
+				i -= 16;
+				continue;
+			}
 			_append(shift + i, result);
 		}
 		assembly {
 			//rewrite to memory
 			let _found := mload(FOUND_SLOT)
 			mstore(FOUND_SLOT, found)
-			found := _found
+			found := shl(_found, sub(0x100, mul(index.depth, 8)))
 		}
+		success = true;
+	}
+
+	function determineRecursionIfGreaterThan(bytes32 ptr) returns (bool res) {
+		Locals memory locals;
+		assembly {
+			locals := ptr
+		}
+		if (
+			locals.result == 0xff ||
+			(locals.ignoreCurrent && (locals.result < locals.next))
+		) {
+			if (locals.recursable) {
+				locals.recursionStarted = true;
+				locals.ignoreCurrent = false;
+				return true;
+			} else {
+				locals.result = locals.next;
+			}
+		} else {
+			if (!locals.recursionStarted) locals.ignoreCurrent = true;
+		}
+		if (locals.result > locals.next) locals.ignoreCurrent = true;
+	}
+
+	function determineRecursableGreaterThan(
+		bytes32 mask,
+		uint256 bound,
+		uint256 i
+	) {
+		if (uint256(mask) ^ bound < bound) {
+			return (true, i - 8);
+		}
+		return (false, 0);
 	}
 }
